8929e25e3fecdd2ac455b9643dfabd550021dc99
Maxim Lapunin <majagyage@gmail.com>
2014-06-05 23:06:25 +0400

    Change gpu drivers to hammerhead from qualcomm site

--- qcom-adreno-330.mk
+++ qcom-adreno-330.mk
@@ -13,32 +13,23 @@
 # limitations under the License.
 
 PRODUCT_COPY_FILES += \
-    vendor/zte/nx503a/adreno330/etc/firmware/a330_pfp.fw:system/etc/firmware/a330_pfp.fw \
-    vendor/zte/nx503a/adreno330/etc/firmware/a330_pm4.fw:system/etc/firmware/a330_pm4.fw \
-    vendor/zte/nx503a/adreno330/vendor/lib/egl/eglsubAndroid.so:system/vendor/lib/egl/eglsubAndroid.so \
-    vendor/zte/nx503a/adreno330/vendor/lib/egl/libEGL_adreno.so:system/vendor/lib/egl/libEGL_adreno.so \
-    vendor/zte/nx503a/adreno330/vendor/lib/egl/libGLESv1_CM_adreno.so:system/vendor/lib/egl/libGLESv1_CM_adreno.so \
-    vendor/zte/nx503a/adreno330/vendor/lib/egl/libGLESv2_adreno.so:system/vendor/lib/egl/libGLESv2_adreno.so \
-    vendor/zte/nx503a/adreno330/vendor/lib/egl/libq3dtools_adreno.so:system/vendor/lib/egl/libq3dtools_adreno.so \
-    vendor/zte/nx503a/adreno330/vendor/lib/libadreno_utils.so:system/vendor/lib/libadreno_utils.so \
-    vendor/zte/nx503a/adreno330/vendor/lib/libC2D2.so:system/vendor/lib/libC2D2.so \
-    vendor/zte/nx503a/adreno330/vendor/lib/libc2d2_z180.so:system/vendor/lib/libc2d2_z180.so \
-    vendor/zte/nx503a/adreno330/vendor/lib/libc2d30-a3xx.so:system/vendor/lib/libc2d30-a3xx.so \
-    vendor/zte/nx503a/adreno330/vendor/lib/libc2d30-a4xx.so:system/vendor/lib/libc2d30-a4xx.so \
-    vendor/zte/nx503a/adreno330/vendor/lib/libc2d30.so:system/vendor/lib/libc2d30.so \
-    vendor/zte/nx503a/adreno330/vendor/lib/libCB.so:system/vendor/lib/libCB.so \
-    vendor/zte/nx503a/adreno330/vendor/lib/libgsl.so:system/vendor/lib/libgsl.so \
-    vendor/zte/nx503a/adreno330/vendor/lib/libllvm-qcom.so:system/vendor/lib/libllvm-qcom.so \
-    vendor/zte/nx503a/adreno330/vendor/lib/libOpenCL.so:system/vendor/lib/libOpenCL.so \
-    vendor/zte/nx503a/adreno330/vendor/lib/libOpenVG.so:system/vendor/lib/libOpenVG.so \
-    vendor/zte/nx503a/adreno330/vendor/lib/librs_adreno_sha1.so:system/vendor/lib/librs_adreno_sha1.so \
-    vendor/zte/nx503a/adreno330/vendor/lib/librs_adreno.so:system/vendor/lib/librs_adreno.so \
-    vendor/zte/nx503a/adreno330/vendor/lib/libRSDriver_adreno.so:system/vendor/lib/libRSDriver_adreno.so \
-    vendor/zte/nx503a/adreno330/vendor/lib/libsc-a2xx.so:system/vendor/lib/libsc-a2xx.so \
-    vendor/zte/nx503a/adreno330/vendor/lib/libsc-a3xx.so:system/vendor/lib/libsc-a3xx.so
-
-#    vendor/zte/nx503a/adreno330/etc/firmware/a225p5_pm4.fw:system/etc/firmware/a225p5_pm4.fw \
-#    vendor/zte/nx503a/adreno330/etc/firmware/a225_pfp.fw:system/etc/firmware/a225_pfp.fw \
-#    vendor/zte/nx503a/adreno330/etc/firmware/a225_pm4.fw:system/etc/firmware/a225_pm4.fw \
-#    vendor/zte/nx503a/adreno330/etc/firmware/a300_pfp.fw:system/etc/firmware/a300_pfp.fw \
-#    vendor/zte/nx503a/adreno330/etc/firmware/a300_pm4.fw:system/etc/firmware/a300_pm4.fw \
+    vendor/zte/nx503a/adreno-hammerhead/etc/firmware/a330_pfp.fw:system/etc/firmware/a330_pfp.fw \
+    vendor/zte/nx503a/adreno-hammerhead/etc/firmware/a330_pm4.fw:system/etc/firmware/a330_pm4.fw \
+    vendor/zte/nx503a/adreno-hammerhead/vendor/lib/egl/eglsubAndroid.so:system/vendor/lib/egl/eglsubAndroid.so \
+    vendor/zte/nx503a/adreno-hammerhead/vendor/lib/egl/libEGL_adreno.so:system/vendor/lib/egl/libEGL_adreno.so \
+    vendor/zte/nx503a/adreno-hammerhead/vendor/lib/egl/libGLESv1_CM_adreno.so:system/vendor/lib/egl/libGLESv1_CM_adreno.so \
+    vendor/zte/nx503a/adreno-hammerhead/vendor/lib/egl/libGLESv2_adreno.so:system/vendor/lib/egl/libGLESv2_adreno.so \
+    vendor/zte/nx503a/adreno-hammerhead/vendor/lib/egl/libplayback_adreno.so:system/vendor/lib/egl/libplayback_adreno.so \
+    vendor/zte/nx503a/adreno-hammerhead/vendor/lib/egl/libq3dtools_adreno.so:system/vendor/lib/egl/libq3dtools_adreno.so \
+    vendor/zte/nx503a/adreno-hammerhead/vendor/lib/libC2D2.so:system/vendor/lib/libC2D2.so \
+    vendor/zte/nx503a/adreno-hammerhead/vendor/lib/libCB.so:system/vendor/lib/libCB.so \
+    vendor/zte/nx503a/adreno-hammerhead/vendor/lib/libOpenCL.so:system/vendor/lib/libOpenCL.so \
+    vendor/zte/nx503a/adreno-hammerhead/vendor/lib/libRSDriver_adreno.so:system/vendor/lib/libRSDriver_adreno.so \
+    vendor/zte/nx503a/adreno-hammerhead/vendor/lib/libadreno_utils.so:system/vendor/lib/libadreno_utils.so \
+    vendor/zte/nx503a/adreno-hammerhead/vendor/lib/libc2d30-a3xx.so:system/vendor/lib/libc2d30-a3xx.so \
+    vendor/zte/nx503a/adreno-hammerhead/vendor/lib/libgsl.so:system/vendor/lib/libgsl.so \
+    vendor/zte/nx503a/adreno-hammerhead/vendor/lib/libllvm-qcom.so:system/vendor/lib/libllvm-qcom.so \
+    vendor/zte/nx503a/adreno-hammerhead/vendor/lib/librs_adreno.so:system/vendor/lib/librs_adreno.so \
+    vendor/zte/nx503a/adreno-hammerhead/vendor/lib/librs_adreno_sha1.so:system/vendor/lib/librs_adreno_sha1.so \
+    vendor/zte/nx503a/adreno-hammerhead/vendor/lib/libsc-a3xx.so:system/vendor/lib/libsc-a3xx.so
+    vendor/zte/nx503a/adreno-hammerhead/vendor/lib/libOpenVG.so:system/vendor/lib/libOpenVG.so \
