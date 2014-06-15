ea8dc07970f7962fc8aab96ce7c21e2ee8010b61
Maxim Lapunin <majagyage@gmail.com>
2014-06-15 15:55:00 +0400

    Adreno drivers from UI2.0 beta 202

--- qcom-adreno-330.mk
+++ qcom-adreno-330.mk
@@ -27,19 +27,19 @@ PRODUCT_COPY_FILES += \
     vendor/zte/nx503a/adreno-330/vendor/lib/egl/libGLESv1_CM_adreno.so:system/vendor/lib/egl/libGLESv1_CM_adreno.so \
     vendor/zte/nx503a/adreno-330/vendor/lib/egl/libGLESv2_adreno.so:system/vendor/lib/egl/libGLESv2_adreno.so \
     vendor/zte/nx503a/adreno-330/vendor/lib/egl/libq3dtools_adreno.so:system/vendor/lib/egl/libq3dtools_adreno.so \
-    vendor/zte/nx503a/adreno-330/vendor/lib/libadreno_utils.so:system/vendor/lib/libadreno_utils.so \
     vendor/zte/nx503a/adreno-330/vendor/lib/libC2D2.so:system/vendor/lib/libC2D2.so \
-    vendor/zte/nx503a/adreno-330/vendor/lib/libc2d2_z180.so:system/vendor/lib/libc2d2_z180.so \
-    vendor/zte/nx503a/adreno-330/vendor/lib/libc2d30-a3xx.so:system/vendor/lib/libc2d30-a3xx.so \
-    vendor/zte/nx503a/adreno-330/vendor/lib/libc2d30-a4xx.so:system/vendor/lib/libc2d30-a4xx.so \
-    vendor/zte/nx503a/adreno-330/vendor/lib/libc2d30.so:system/vendor/lib/libc2d30.so \
     vendor/zte/nx503a/adreno-330/vendor/lib/libCB.so:system/vendor/lib/libCB.so \
-    vendor/zte/nx503a/adreno-330/vendor/lib/libgsl.so:system/vendor/lib/libgsl.so \
-    vendor/zte/nx503a/adreno-330/vendor/lib/libllvm-qcom.so:system/vendor/lib/libllvm-qcom.so \
     vendor/zte/nx503a/adreno-330/vendor/lib/libOpenCL.so:system/vendor/lib/libOpenCL.so \
     vendor/zte/nx503a/adreno-330/vendor/lib/libOpenVG.so:system/vendor/lib/libOpenVG.so \
-    vendor/zte/nx503a/adreno-330/vendor/lib/librs_adreno_sha1.so:system/vendor/lib/librs_adreno_sha1.so \
-    vendor/zte/nx503a/adreno-330/vendor/lib/librs_adreno.so:system/vendor/lib/librs_adreno.so \
     vendor/zte/nx503a/adreno-330/vendor/lib/libRSDriver_adreno.so:system/vendor/lib/libRSDriver_adreno.so \
+    vendor/zte/nx503a/adreno-330/vendor/lib/libadreno_utils.so:system/vendor/lib/libadreno_utils.so \
+    vendor/zte/nx503a/adreno-330/vendor/lib/libc2d2_z180.so:system/vendor/lib/libc2d2_z180.so \
+    vendor/zte/nx503a/adreno-330/vendor/lib/libc2d30.so:system/vendor/lib/libc2d30.so \
+    vendor/zte/nx503a/adreno-330/vendor/lib/libc2d30-a3xx.so:system/vendor/lib/libc2d30-a3xx.so \
+    vendor/zte/nx503a/adreno-330/vendor/lib/libc2d30-a4xx.so:system/vendor/lib/libc2d30-a4xx.so \
+    vendor/zte/nx503a/adreno-330/vendor/lib/libgsl.so:system/vendor/lib/libgsl.so \
+    vendor/zte/nx503a/adreno-330/vendor/lib/libllvm-qcom.so:system/vendor/lib/libllvm-qcom.so \
+    vendor/zte/nx503a/adreno-330/vendor/lib/librs_adreno.so:system/vendor/lib/librs_adreno.so \
+    vendor/zte/nx503a/adreno-330/vendor/lib/librs_adreno_sha1.so:system/vendor/lib/librs_adreno_sha1.so \
     vendor/zte/nx503a/adreno-330/vendor/lib/libsc-a2xx.so:system/vendor/lib/libsc-a2xx.so \
     vendor/zte/nx503a/adreno-330/vendor/lib/libsc-a3xx.so:system/vendor/lib/libsc-a3xx.so
