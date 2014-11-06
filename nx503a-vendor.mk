fd919bbbe29495a50db3d95adb77571443150250
Maxim Lapunin <majagyage@gmail.com>
2014-11-07 00:16:51 +0300

    Remove binary dalvik and add oem application for IR control

--- nx503a-vendor.mk
+++ nx503a-vendor.mk
@@ -35,7 +35,7 @@ $(call inherit-product, vendor/zte/nx503a/gapps.mk)
 # Qualcomm Adreno 330 drivers update
 $(call inherit-product, vendor/zte/nx503a/qcom-adreno-330.mk)
 
-# Optimized dalvik & bionic for Qualcomm SoC
-$(call inherit-product, vendor/zte/nx503a/qcom-dalvik.mk)
+# OEM applications
+$(call inherit-product, vendor/zte/nx503a/oem.mk)
 
 $(call inherit-product, vendor/zte/nx503a/nx503a-vendor-blobs.mk)
